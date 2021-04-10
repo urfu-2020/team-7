a06019506f19a4368e0a27cea209339b779a427a
matthewsavelev <savelevmatthew@gmail.com>
2021-04-10 15:24:47 +0500

    - Errors handler created!

--- Kilogram/routes.js
+++ Kilogram/routes.js
@@ -1,8 +1,8 @@
+const { error404 } = require('./api/errors');
+
 module.exports = (app) => {
   app.get('/', (req, res) => {
     res.send('<h1>Hello, world!</h1>');
   });
-  app.all('*', (req, res) => {
-    res.sendStatus(404);
-  });
+  app.all('*', error404);
 };
